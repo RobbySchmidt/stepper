067f0dff63f2fb18330c50df1f5c01a449743ca0
RobbySchmidt <168719558+RobbySchmidt@users.noreply.github.com>
2025-10-16 13:11:10 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  devtools: { enabled: false },
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container mx-auto px-4 py-12">
+    <div class="max-w-3xl mx-auto">
+      <template v-if="step === 1">
+        <form 
+          @submit.prevent="getName"
+          class="space-y-4">
+          <Label 
+            for="firstName">
+            First Name
+          </Label>
+          <Input 
+            type="text" 
+            v-model="firstName" />
+          <Button 
+            type="submit"
+            class="duration-300 ease-in-out cursor-pointer bg-green-500 hover:bg-green-500/80">
+            next
+          </Button>
+        </form>
+      </template>
+      <template v-else-if="step === 2">
+         <form 
+          @submit.prevent="getTask"
+          class="space-y-4">
+          <Label 
+            for="lastName">
+            Last Name
+          </Label>
+          <Input 
+            type="text" 
+            v-model="lastName" />
+          <Button 
+            type="submit"
+            class="duration-300 ease-in-out cursor-pointer bg-green-500 hover:bg-green-500/80">
+            next
+          </Button>
+        </form>
+      </template>
+      <template v-else>
+        <span>submitted Name: {{ values.firstName }} {{ values.lastName }}</span>
+      </template>
+    </div>
+    <pre>{{ values }}</pre>
+  </div>
+</template>
+
+<script setup>
+  const step = ref(1)
+
+  const firstName = ref('')
+  const lastName = ref('')
+
+  const values = reactive({})
+
+  function getName() {
+    if(firstName.value) {
+      values.firstName = firstName.value
+      firstName.value = ''
+      step.value = 2
+    }
+  }
+
+  function getTask() {
+    if(lastName.value) {
+      values.lastName = lastName.value
+      lastName.value = ''
+      step.value = 3
+    }
+  }
+
+</script>
+
+<style scoped>
+
+</style>
